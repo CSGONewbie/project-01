4bd71cb518d1a40ccfcef0846f2aa84c9215e51d
Omar Meza <omeza3.14@hotmail.com>
2020-11-03 14:28:51 -0500

    Agregando carpeta js con archivos javascript

---
+++ js/librerias.js
@@ -0,0 +1,7 @@
+var SIZE_IMG = 300;
+var arr_img = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+
+for (let i = 0; i < array.length; i++) 
+{
+     console.log(arr_img[i]);
+}
---
+++ js/utilidades.js
@@ -0,0 +1,1 @@
+var str_v = "HOLA MUNDO!!"
