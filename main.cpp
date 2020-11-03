fd7e80a129aa64bccbde9c8840f79cbe0a79c871
Omar Meza <omeza3.14@hotmail.com>
2020-11-03 10:10:36 -0500

    Agregando LICENSE README main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 3;
+    double mat_d[3][3];
+
+    for(int i=0; i < size; i++ )
+    for(int j=0; j < size; j++)
+    {
+        mat_d[i][j] = 0.0;
+    }
+
+    
+    return 0;
+}
